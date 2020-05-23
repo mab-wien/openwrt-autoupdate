bd9f6a9242c4f54fef1e66ea56ba2a044cca9b98
easyinternetat <github@easyinternet.at>
2020-05-23 19:29:03 +0200

    Reformat code

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -1,76 +1,77 @@
-#!/bin/sh
-CONFIG_PATH="/etc/auto-update.conf";
+#!/bin/bash
+CONFIG_PATH="/etc/auto-update.conf"
 
-function checkAndSetConfig {
+function checkAndSetConfig() {
   if [ ! -f "$CONFIG_PATH" ]; then
-      if [ "$1" == "" ]; then
-        echo "Config not found ($CONFIG_PATH)";
-        exit;
-      fi
-      echo "Create Config ($CONFIG_PATH)"
-      touch $CONFIG_PATH;
-      echo USER_PACKAGES=\"$@\" >> $CONFIG_PATH;
-      cat $CONFIG_PATH;
+    if [ "$1" == "" ]; then
+      echo "Config not found ($CONFIG_PATH)"
+      exit
+    fi
+    echo "Create Config ($CONFIG_PATH)"
+    touch $CONFIG_PATH
+    # shellcheck disable=SC2145
+    echo "USER_PACKAGES=\"$@\"" >>$CONFIG_PATH
+    cat $CONFIG_PATH
   fi
-  grep -c $CONFIG_PATH /etc/sysupgrade.conf >> /dev/null
-  if [ "$?" != "0" ]
-  then
-    echo $CONFIG_PATH >> /etc/sysupgrade.conf;
+  grep -c $CONFIG_PATH /etc/sysupgrade.conf >>/dev/null
+  # shellcheck disable=SC2181
+  if [ "$?" != "0" ]; then
+    echo $CONFIG_PATH >>/etc/sysupgrade.conf
   fi
 }
 
-checkAndSetConfig $@;
+# shellcheck disable=SC2068
+checkAndSetConfig $@
 . /etc/os-release
-. $CONFIG_PATH;
+# shellcheck disable=SC1090
+. $CONFIG_PATH
 echo "Hostname: $(grep hostname /etc/config/system | awk '{print $3}')"
-echo "System Version: $VERSION";
-echo "Target: $OPENWRT_BOARD";
-MODEL="$(cat /etc/board.json  |jsonfilter -e '@.model.id' | tr ',' '_')"
-echo "Model: $MODEL";
+echo "System Version: $VERSION"
+echo "Target: $OPENWRT_BOARD"
+MODEL="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
+echo "Model: $MODEL"
 
 opkg update
 opkg install libustream-openssl
-CURRENT_VERSION="$(wget --no-check-certificate -q https://downloads.openwrt.org/releases/ -O - |grep -E '<a href="[0-9]+.[0-9]+.[0-9]+/">'|awk -F '</a>' '{print $1}'|awk -F '>' '{print $(NF)}'|sort -n -r|head -1)";
-if [ "$?" != "0" ]
-then
-	echo "wget error";
-	exit;
+CURRENT_VERSION="$(wget --no-check-certificate -q https://downloads.openwrt.org/releases/ -O - | grep -E '<a href="[0-9]+.[0-9]+.[0-9]+/">' | awk -F '</a>' '{print $1}' | awk -F '>' '{print $(NF)}' | sort -n -r | head -1)"
+# shellcheck disable=SC2181
+if [ "$?" != "0" ]; then
+  echo "wget error"
+  exit
 fi
-if [ "$CURRENT_VERSION" == "" ]
-then
-	echo "error: current openwrt version not found";
-	exit;
+if [ "$CURRENT_VERSION" == "" ]; then
+  echo "error: current openwrt version not found"
+  exit
 fi
-echo "Current Realease: $CURRENT_VERSION";
-if [ "$CURRENT_VERSION" == "$VERSION" ]
-then
-  echo "System up-to-date";
-  if [ "$USER_PACKAGES" != "" ]
-  then
+echo "Current Release: $CURRENT_VERSION"
+if [ "$CURRENT_VERSION" == "$VERSION" ]; then
+  echo "System up-to-date"
+  if [ "$USER_PACKAGES" != "" ]; then
     #opkg update
-    opkg install $USER_PACKAGES;
+    # shellcheck disable=SC2086
+    opkg install $USER_PACKAGES
   fi
-  opkg list-upgradable;
-  echo "Packages up-to-date";
-	exit;
+  opkg list-upgradable
+  echo "Packages up-to-date"
+  exit
 fi
-echo "sysupgrade: $VERSION => $CURRENT_VERSION";
+echo "sys-upgrade: $VERSION => $CURRENT_VERSION"
 
-FILENAME="openwrt-$CURRENT_VERSION-$(echo $OPENWRT_BOARD | tr '/' '-' )-$MODEL-squashfs-sysupgrade.bin"
-BASE_LINK="https://downloads.openwrt.org/releases/$CURRENT_VERSION/targets/$(echo $OPENWRT_BOARD | tr '-' '/' )/";
-SHA256SUMS=$(wget --no-check-certificate $BASE_LINK/sha256sums -q -O -|grep $FILENAME | awk '{print $1}')
-TARGET_PATH="/tmp/$FILENAME";
-wget --no-check-certificate $BASE_LINK$FILENAME -O $TARGET_PATH
-if [ "$?" != "0" ]
-then
-	echo "download error ($BASE_LINK$FILENAME)";
-	exit;
+FILENAME="openwrt-$CURRENT_VERSION-$(echo "$OPENWRT_BOARD" | tr '/' '-')-$MODEL-squashfs-sysupgrade.bin"
+BASE_LINK="https://downloads.openwrt.org/releases/$CURRENT_VERSION/targets/$(echo "$OPENWRT_BOARD" | tr '-' '/')/"
+SHA256SUMS=$(wget --no-check-certificate "$BASE_LINK/sha256sums" -q -O - | grep "$FILENAME" | awk '{print $1}')
+TARGET_PATH="/tmp/$FILENAME"
+wget --no-check-certificate "$BASE_LINK$FILENAME" -O "$TARGET_PATH"
+# shellcheck disable=SC2181
+if [ "$?" != "0" ]; then
+  echo "download error ($BASE_LINK$FILENAME)"
+  exit
 fi
-sha256sum $TARGET_PATH | grep $SHA256SUMS
-if [ "$?" != "0" ]
-then
-	echo "sha256sum error ($SHA256SUMS)";
-	exit;
+sha256sum "$TARGET_PATH" | grep "$SHA256SUMS"
+# shellcheck disable=SC2181
+if [ "$?" != "0" ]; then
+  echo "sha256sum error ($SHA256SUMS)"
+  exit
 fi
 
-sysupgrade -v $TARGET_PATH;
+sysupgrade -v "$TARGET_PATH"
--- bin/remote-auto-update.sh
+++ bin/remote-auto-update.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [ "$1" == "" ] || [ "$2" == "" ]  || [ "$4" != "" ]
-then
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$4" != "" ]; then
   echo "usage $0 'host1 host2 host3' 'curl tcpdump' ['sleep 30 && reboot'] "
-  exit;
+  exit
 fi
 
-OpenWrtDevices="$1";
-USER_PACKAGES="$2";
-EXTRA_COMMAND="$3";
-TMP_FILENAME_PATH="/tmp/openwrt-auto-update.sh";
+OpenWrtDevices="$1"
+USER_PACKAGES="$2"
+EXTRA_COMMAND="$3"
+TMP_FILENAME_PATH="/tmp/openwrt-auto-update.sh"
 
 cd /tmp/ || exit
 wget https://raw.githubusercontent.com/easyinternetat/openwrt-autoupdate/master/bin/auto-update.sh -O $TMP_FILENAME_PATH
-for host in $OpenWrtDevices
-do
-  echo "Connect to $host";
-  ssh -oStrictHostKeyChecking=no "$host" "sh -s $USER_PACKAGES" < $TMP_FILENAME_PATH
-  if [ "$3" != "" ]
-  then
-    ssh -oStrictHostKeyChecking=no "$host" "$EXTRA_COMMAND";
+for host in $OpenWrtDevices; do
+  echo "Connect to $host"
+  ssh -oStrictHostKeyChecking=no "$host" "sh -s $USER_PACKAGES" <$TMP_FILENAME_PATH
+  if [ "$3" != "" ]; then
+    ssh -oStrictHostKeyChecking=no "$host" "$EXTRA_COMMAND"
   fi
 done
